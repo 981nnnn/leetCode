15a0a06460f2d9b99d03b5a0b5f2c4b8a3f536ea
wangxiaobao <wangxiaobao@hd123.com>
2021-01-29 15:51:24 +0800

    title: <feat> 通过删除字母匹配到字典里的最长单词

---
+++ code/src/LongestWordInDictionaryThroughDeleting524.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author wangxiaobao
+ */
+public class LongestWordInDictionaryThroughDeleting524 {
+
+
+    public static String findLongestWord(String s, List<String> d) {
+        String flag = "";
+        for (int i = 0; i < d.size(); i++) {
+            String str = findStr(d.get(i),targetList(s));
+            if(str.length()>flag.length()){
+                flag = str;
+            }
+        }
+        return flag;
+    }
+
+    private static String findStr( String listString, ArrayList<Character> characters){
+        char[] tarGetChar = listString.toCharArray();
+
+        int i = 0;
+        int j = 0;
+        while (i<listString.length()){
+            if(tarGetChar[i]==characters.get(j)){
+                characters.remove(j);
+                i++;
+                j=0;
+            }else{
+                j++;
+            }
+            if(j==characters.size()){
+                return "";
+            }
+        }
+
+        return listString;
+    }
+    private static ArrayList<Character> targetList(String target){
+        char[] tarGetChar = target.toCharArray();
+        ArrayList<Character> characters = new ArrayList<>();
+        for (int i = 0; i < tarGetChar.length; i++) {
+            characters.add(tarGetChar[i]);
+        }
+        return characters;
+    }
+
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("ale");
+        list.add("apple");
+        list.add("monkey");
+        list.add("plea");
+        String string = "abpcplea";
+        String longestWord = findLongestWord(string, list);
+        System.out.println(longestWord);
+    }
+}
