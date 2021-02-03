7ff708d68a17d524af8dcda9372e6b6ef8928bf0
wangxiaobao <wangxiaobao@hd123.com>
2021-02-03 10:42:32 +0800

    title: <feat> 通过删除字母匹配到字典里的最长单词

--- code/src/LongestWordInDictionaryThroughDeleting524.java
+++ code/src/LongestWordInDictionaryThroughDeleting524.java
@@ -9,53 +9,49 @@ public class LongestWordInDictionaryThroughDeleting524 {
 
 
     public static String findLongestWord(String s, List<String> d) {
-        String flag = "";
-        for (int i = 0; i < d.size(); i++) {
-            String str = findStr(d.get(i),targetList(s));
-            if(str.length()>flag.length()){
-                flag = str;
+        String longstr = "";
+        for (String targetStr : d){
+            int l1 = longstr.length();
+            int l2 = targetStr.length();
+            if(l1>l2 || (l1==l2 & longstr.compareTo(targetStr)<0)){
+                continue;
+            }
+            if(findStr(s,targetStr)){
+                longstr = targetStr;
             }
         }
-        return flag;
+        return longstr;
     }
 
-    private static String findStr( String listString, ArrayList<Character> characters){
-        char[] tarGetChar = listString.toCharArray();
-
+    /**
+     * 判断str 中是否在sourceString中
+     * @param sourceString  主函数传递的str
+     * @param targetString  list中的str
+     * @return
+     */
+    private static Boolean findStr( String sourceString, String targetString){
         int i = 0;
         int j = 0;
-        while (i<listString.length()){
-            if(tarGetChar[i]==characters.get(j)){
-                characters.remove(j);
-                i++;
-                j=0;
-            }else{
+        while(i<sourceString.length() && j < targetString.length()){
+            if(sourceString.charAt(i) == targetString.charAt(j)){
                 j++;
             }
-            if(j==characters.size()){
-                return "";
-            }
+            i++;
         }
-
-        return listString;
-    }
-    private static ArrayList<Character> targetList(String target){
-        char[] tarGetChar = target.toCharArray();
-        ArrayList<Character> characters = new ArrayList<>();
-        for (int i = 0; i < tarGetChar.length; i++) {
-            characters.add(tarGetChar[i]);
-        }
-        return characters;
+        // 如果返回true，target在sourceString 中
+        return j == targetString.length();
     }
 
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
-        list.add("ale");
         list.add("apple");
-        list.add("monkey");
-        list.add("plea");
-        String string = "abpcplea";
+        list.add("ewaf");
+        list.add("awefawfwaf");
+        list.add("awef");
+        list.add("awefe");
+        list.add("ewafeffewafewf");
+        String string = "aewfafwafjlwajflwajflwafj";
         String longestWord = findLongestWord(string, list);
         System.out.println(longestWord);
     }
